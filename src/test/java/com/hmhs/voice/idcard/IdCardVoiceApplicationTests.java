b25bc4e1ebde53b725f0e9333dfe93cb05936b20
Yogesh Sharma <yogesh4004@example.com>
2020-09-01 11:36:46 -0400

    fixing app name and Junit

--- src/test/java/com/hmhs/voice/idcard/IdCardVoiceApplicationTests.java
+++ src/test/java/com/hmhs/voice/idcard/IdCardVoiceApplicationTests.java
@@ -1,13 +1,13 @@
 package com.hmhs.voice.idcard;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class IdCardVoiceApplicationTests {
+class IdCardVoiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
